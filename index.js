082388059332ce642d243871f06cae570aea10ea
Kashav Madan <kshvmdn@gmail.com>
2016-01-22 23:22:43 -0500

    Add mongoose

--- index.js
+++ index.js
@@ -5,35 +5,31 @@ const config = require('./config');
 const login = require('facebook-chat-api');
 const client = require('twilio')(config.twilio.accountSid, config.twilio.authToken);
 
+const FacebotModel = require('./Schema');
+
+
 login({email: config.fb.email, password: config.fb.pass}, function callback(err, api) {
   if (err) return console.error(err);
 
   var listen = api.listen(function callback(err, message) {
     if (err) return console.error(err);
 
-    var friends = [];
-
     var body = message.body.toLowerCase(); 
     var sender = message.senderID;
 
     if (body.includes('@facebot ')) {
-
       body = body.slice('@facebot '.length);
-
       if (body.includes('register ')) {
         console.log('register called');
-
         body = body.slice('register '.length).split('`');
         var name = body[0].trim();
         var number = body[1];
-
+        new FacebotModel({name: name, number: number}).save();
       } else if (body.includes('send ')) {
         console.log('send called');
-
         body = body.slice('send '.length).split('`');
         var name = body[0].trim();
         var msg = body[1];
-        
       }
     }
   });
