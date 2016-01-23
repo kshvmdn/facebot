fecdb8b5d7d38626b0db00f09366fe6e3c7ed445
Kashav Madan <kshvmdn@gmail.com>
2016-01-23 17:34:44 -0500

    Fix errors

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ const Messages = require('./models/messages');
 const Threads = require('./models/threads');
 
 login({email: config.fb.email, password: config.fb.pass}, function callback(err, api) {
-  if (err) return HandleError(err);
+  if (err) return Error(err);
 
   var listen = api.listen(function callback(err, event) {
-    if (err) return HandleError(err);
+    if (err) return Error(err);
 
     var body = event.body.toLowerCase(); 
 
@@ -25,23 +25,23 @@ login({email: config.fb.email, password: config.fb.pass}, function callback(err,
       if (body.includes('register ')) {
         body = body.slice('register '.length).split('`');
         Person.create({name: body[0].trim(), number: body[1]}, function(err, person) {
-          if (err) return HandleError(err);
+          if (err) return Error(err);
           else console.log('New user added')
         });
       } else if (body.includes('send ')) {
         body = body.slice('send '.length).split('`');
         Person.findOne({name: body[0].trim()}, function(err, person) {
-          if (err || person == null) return HandleError(err);
+          if (err || person == null) return Error(err);
           twilioClient.sms.messages.create({
             body: body[1],
             to: person.number,
             from: config.twilio.number
           }, function(err, sms) {
-            if (err) return HandleError(err);
+            if (err) return Error(err);
             Threads.count({ threadID: event.threadID }, function(err, count) {
               if ( count == 0 ) {
                 Threads.create({ threadID: event.threadID, messages: [sms.sid] }, function(err, thread) {
-                  if (err) return HandleError(err);
+                  if (err) return Error(err);
                   else console.log('New thread added');
                 });
               } else {
@@ -50,7 +50,7 @@ login({email: config.fb.email, password: config.fb.pass}, function callback(err,
                   { $push: { messages: sms.sid } },
                   { safe: true, upsert: true },
                   function(err, model) {
-                    if (err) return HandleError(err);
+                    if (err) return Error(err);
                     console.log('Added msg to thread');
                   }
                 )
@@ -64,10 +64,10 @@ login({email: config.fb.email, password: config.fb.pass}, function callback(err,
     // twitter integration
     else if (body.includes('@tweetbot ')) {
       body = body.slice('@tweetbot '.length);
-      if (body.includes('tweet ')) {
+      if (body.includes('tweeet ')) {
         body = body.slice('tweet '.length); // tweet
         twitterClient.post('statuses/update', {status: body},  function(error, tweet, response){
-          if(error) return HandleError(error); 
+          if(error) return Error(error); 
         });
       } else if (body.includes('latest tweet ')) {
         body = body.slice('latest tweet '.length); // username
@@ -85,7 +85,7 @@ login({email: config.fb.email, password: config.fb.pass}, function callback(err,
             var count = (tweet.text.match(/http/g) || []).length;
             var text = tweet.text;
             for (let i = 0; i < count; i++) {
-              // super hacky way of removing links from text, i apologize
+              // super hacky way of removing links from text, i apologize l o l
               text = text.replace(text.slice(text.indexOf('https'), text.indexOf('https') + 24), '').trim()
             }
             api.sendMessage(text, event.threadID);
@@ -105,7 +105,7 @@ login({email: config.fb.email, password: config.fb.pass}, function callback(err,
           Messages.count( { sid: sms.sid }, function(err, count) {
             if ( count == 0 ) {
               Messages.create({sid: sms.sid, threadID: event.threadID, shown: false}, function(err, message) {
-                if (err) return HandleError(err);
+                if (err) return Error(err);
                 else console.log('Message added')
               });
             }
@@ -114,13 +114,13 @@ login({email: config.fb.email, password: config.fb.pass}, function callback(err,
       });
     });
 
-    // send previously-unshown msgs to current thread
+    // send unshown msgs to current fb thread
     Messages.find({ threadID: event.threadID, shown: false }, function(err, messages) {
       if (messages.length > 0) {
         messages.forEach(function(sms) {
           twilioClient.messages(sms.sid).get(function(err, sms1) {
             Person.findOne({ number: sms1.from }, function(err, person) {
-              if (err) return HandleError(err);
+              if (err) return Error(err);
               var name = person == null ? sms.from : person.name;
               api.sendMessage(name + ' says ' + sms1.body + ' [' + sms1.date_sent.slice(0, sms1.date_sent.length-5).trim() + ']', event.threadID);
               Messages.findOneAndUpdate(
@@ -128,7 +128,7 @@ login({email: config.fb.email, password: config.fb.pass}, function callback(err,
                   { $set: { shown: true } },
                   { safe: true, upsert: true },
                   function(err, model) {
-                    if (err) return HandleError(err);
+                    if (err) return Error(err);
                   }
                 )
             });
