b1352d9a4e76fb56b3f629fec5944c1681417d7b
Kashav Madan <kshvmdn@gmail.com>
2016-01-23 13:22:57 -0500

    Config. ngrok

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const app = express();
 
 const PORT = 8080 | process.env.PORT;
 
+const ngrok = require('ngrok');
+const authToken = require('./config').ngrok.authToken;
+
+ngrok.authtoken(authToken, function(err, token) {});
+
+ngrok.connect({proto: 'tcp', addr: 22}, function (err, url) {
+  console.log(url);
+});
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/response.xml');
 });
