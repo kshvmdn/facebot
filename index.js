fb8a3fd85023b86f41254c8090d4b7af83f2801c
Kashav Madan <kshvmdn@gmail.com>
2016-01-23 02:58:53 -0500

    Finalize core Twilio app

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const Person = require('./models/person');
 const Messages = require('./models/messages');
 
 login({email: config.fb.email, password: config.fb.pass}, function callback(err, api) {
-  if (err) return console.error(err);
+  if (err) return HandleError(err);
 
   var listen = api.listen(function callback(err, message) {
-    if (err) return console.error(err);
+    if (err) return HandleError(err);
 
     var threadID = message.threadID;
     var body = message.body.toLowerCase(); 
@@ -22,12 +22,10 @@ login({email: config.fb.email, password: config.fb.pass}, function callback(err,
       body = body.slice('@facebot '.length);
       if (body.includes('register ')) {
         body = body.slice('register '.length).split('`');
-
         Person.create({name: body[0].trim(), number: body[1]}, function(err, person) {
           if (err) return HandleError(err);
           else console.log('New user added')
         });
-
       } else if (body.includes('send ')) {
         body = body.slice('send '.length).split('`');
         Person.findOne({name: body[0].trim()}, function(err, person) {
@@ -37,8 +35,8 @@ login({email: config.fb.email, password: config.fb.pass}, function callback(err,
             to: person.number,
             from: config.twilio.number
           }, function(err, sms) {
-            if (!err) console.log(sms.sid);
-            else console.dir(err);
+            if (err) return HandleError(err);
+            console.log(sms.sid);
           });
         });
       }
@@ -46,16 +44,16 @@ login({email: config.fb.email, password: config.fb.pass}, function callback(err,
     client.messages.list({}, function(err, data) { 
       data.messages.forEach(function(sms) {
         if ( sms.to == config.twilio.number ) {
-
           Messages.count( { sid: sms.sid }, function(err, count) {
             if ( count == 0 ) {
               Person.findOne({number: sms.from}, function(err, person) {
                 if (err) return HandleError(err);
+                // TODO: format date
                 api.sendMessage(person.name + ' says ' + sms.body + ' (' + sms.date_sent + ')', threadID);
               });
               Messages.create({sid: sms.sid}, function(err, message) {
                 if (err) return HandleError(err);
-                else console.log('Message added')
+                console.log('Message added')
               });
             }
           });
