6c8acea4cd70c4f25916b1ddc84b64d9f16444a5
Kashav Madan <kshvmdn@gmail.com>
2016-01-23 05:52:55 -0500

    strict: false

--- models/messages.js
+++ models/messages.js
@@ -3,6 +3,7 @@ const db = mongoose.createConnection('mongodb://localhost/facebot');
 
 const MessageSchema = new mongoose.Schema({
   sid: String,
-});
+  threadId: Number
+},  { strict: false });
 
 module.exports = db.model('messages', MessageSchema);
--- models/person.js
+++ models/person.js
@@ -4,6 +4,6 @@ const db = mongoose.createConnection('mongodb://localhost/facebot');
 const PersonSchema = new mongoose.Schema({
   name: String,
   number: String
-});
+},  { strict: false });
 
 module.exports = db.model('person', PersonSchema);
